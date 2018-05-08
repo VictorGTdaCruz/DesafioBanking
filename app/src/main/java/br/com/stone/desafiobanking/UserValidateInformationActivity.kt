07a434fec9a6a3bf31e0e794d39dfae93203b759
victor.cruz <victor.cruz@stone.com.br>
2018-05-08 16:52:05 -0300

    :tada: Initial commit

---
+++ app/src/main/java/br/com/stone/desafiobanking/UserValidateInformationActivity.kt
@@ -0,0 +1,12 @@
+package br.com.stone.desafiobanking
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class UserValidateInformationActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_user_validate_information)
+    }
+}
