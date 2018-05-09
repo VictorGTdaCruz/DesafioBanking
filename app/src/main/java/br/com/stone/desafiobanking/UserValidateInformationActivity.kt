772fd2488b348cf781b5af4dfd963ff44e201b9e
victor.cruz <victor.cruz@stone.com.br>
2018-05-09 15:14:33 -0300

    :construction: Add listeners

--- app/src/main/java/br/com/stone/desafiobanking/UserValidateInformationActivity.kt
+++ app/src/main/java/br/com/stone/desafiobanking/UserValidateInformationActivity.kt
@@ -2,6 +2,10 @@ package br.com.stone.desafiobanking
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
+import android.view.View
+import kotlinx.android.synthetic.main.activity_user_validate_information.*
 
 class UserValidateInformationActivity : AppCompatActivity() {
 
@@ -9,4 +13,63 @@ class UserValidateInformationActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_user_validate_information)
     }
+
+    override fun onStart() {
+        super.onStart()
+
+        edit_name.addTextChangedListener(editNameOnTextChangedListener())
+        edit_name.addTextChangedListener(editEmailOnTextChangedListener())
+        edit_name.addTextChangedListener(editCPFOnTextChangedListener())
+        btn_confirm.setOnClickListener(btnConfirmOnClickListener())
+
+    }
+
+    private fun editNameOnTextChangedListener(): TextWatcher{
+        return object : TextWatcher{
+            override fun afterTextChanged(p0: Editable?) {
+            }
+
+            override fun beforeTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
+            }
+
+            override fun onTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
+
+            }
+        }
+    }
+
+    private fun editEmailOnTextChangedListener(): TextWatcher{
+        return object : TextWatcher{
+            override fun afterTextChanged(p0: Editable?) {
+            }
+
+            override fun beforeTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
+            }
+
+            override fun onTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
+
+            }
+        }
+    }
+
+    private fun editCPFOnTextChangedListener(): TextWatcher{
+        return object : TextWatcher{
+            override fun afterTextChanged(p0: Editable?) {
+            }
+
+            override fun beforeTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
+            }
+
+            override fun onTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
+
+            }
+        }
+    }
+
+    private fun btnConfirmOnClickListener(): View.OnClickListener{
+        return View.OnClickListener {
+
+        }
+    }
+
 }
